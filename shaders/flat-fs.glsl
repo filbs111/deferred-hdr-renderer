619879e7f5a2805d1f2bc98378bb536103618904
filbs111 <filbs111@protonmail.com>
2026-04-20 02:35:35 +0100

    point light

--- shaders/flat-fs.glsl
+++ shaders/flat-fs.glsl
@@ -2,17 +2,36 @@
 precision mediump float;
 
 out vec4 fragColor;
-in vec3 vLightTimesColor;
+in vec3 vLight;
+
+in vec3 fromPointLight;
+in vec3 normalCopy;
+
+uniform vec3 uFlatColor;
+
 
 void main(void) {
 
     float lightMultiplier = 1.;
 
+    vec3 regularLight = lightMultiplier*vLight;
+
+    regularLight = regularLight*0.;
+
+    float dotProd = max( -dot(fromPointLight, normalCopy) , 0.);
+    float distSq = dot(fromPointLight, fromPointLight);
+
+    vec3 pointLightColor = vec3(.5);
+    
+    vec3 pointLightContrib = pointLightColor*dotProd/distSq;
+
+    vec3 totalLight = uFlatColor * (regularLight + pointLightContrib);
+
 #ifdef HDR
-    vec3 hdrified = 1. - exp(-lightMultiplier*vLightTimesColor);
+    vec3 hdrified = 1. - exp(-totalLight);
     fragColor = vec4(pow(hdrified, vec3(0.455)),1.0);
 #else
-    fragColor = vec4(pow(lightMultiplier*vLightTimesColor, vec3(0.455)),1.0);
+    fragColor = vec4(pow(totalLight, vec3(0.455)),1.0);
 #endif
 
 }
--- shaders/flat-vs.glsl
+++ shaders/flat-vs.glsl
@@ -6,16 +6,28 @@ uniform mat4 uVMatrix;
 uniform mat4 uMMatrix;
 uniform mat4 uPMatrix;
 
-uniform vec3 uFlatColor;
-out vec3 vLightTimesColor;
+out vec3 vLight;
+
+//TODO put into normal frame with tangent vec etc? 
+out vec3 fromPointLight;
+out vec3 normalCopy;
+
 
 void main(void) {
-    gl_Position = uPMatrix * uVMatrix*uMMatrix * vec4(aVertexPosition, 1.0);
+
+    vec4 transformedPosition = uMMatrix * vec4(aVertexPosition, 1.0);
+
+    gl_Position = uPMatrix * uVMatrix*transformedPosition;
 
     //NOTE this is wierd because model matrix includes scale!
     // NOTE normalizing vector maybe is wrong for non-uniformly scaled objects. TODO fix, or just use unscaled objects.
     vec4 transformedNormal = (uMMatrix * vec4(aVertexNormal, 0.0));
     float light = 0.5+0.5*dot(normalize(transformedNormal), vec4(0.,1.,0.,0.));
     
-    vLightTimesColor = uFlatColor * vec3(light);
+    vLight = vec3(light);
+
+
+    normalCopy = transformedNormal.xyz;
+    vec3 light1Pos = vec3(0.,-2.,-4.7);
+    fromPointLight = transformedPosition.xyz - light1Pos;
 }
